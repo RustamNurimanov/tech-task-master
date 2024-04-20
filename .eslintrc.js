7e625a85997a3e1e4dcbae0ad5d98337700bb0f5
rustamnurimanov <r.damirovich@gmail.com>
2024-04-20 13:06:28 +0300

    [chore] added rules to eslint + pckg with loaders & responsive hook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,130 +1,199 @@
 module.exports = {
-    "root": true,
-    "env": {
-        "browser": true,
-        "es2021": true
+  root: true,
+
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+    es6: true,
+  },
+
+  settings: {
+    react: {
+      version: "latest",
     },
-    "extends": [
-        "standard-with-typescript",
-        "plugin:react/recommended",
-        "eslint:recommended",
-        'plugin:sonarjs/recommended',
+  },
+
+  rules: {
+    "linebreak-style": "off", // Неправильно работает в Windows.
+    "implicit-arrow-linebreak": "off",
+    "arrow-parens": "off", // Несовместимо с prettier
+    "object-curly-newline": "off", // Несовместимо с prettier
+    "arrow-body-style": "off",
+    "function-paren-newline": "off", // Несовместимо с prettier
+    "space-before-function-paren": "off", // Несовместимо с prettier
+    "class-methods-use-this": "off",
+    "operator-linebreak": "off",
+    "max-classes-per-file": "off",
+    "consistent-return": "off",
+    "prefer-const": "warn",
+    "dot-notation": "warn",
+    "array-callback-return": "warn",
+    "guard-for-in": "warn",
+    "prefer-template": "warn",
+    "new-cap": "warn",
+    "prefer-object-spread": "warn",
+    "prefer-arrow-callback": "off",
+    "comma-dangle": "off",
+    "lines-between-class-members": "off", // не позволяет группировать поля в классах
+    "prefer-destructuring": "off",
+    "wrap-iife": "off",
+
+    // NO-RULES
+    "no-mixed-operators": "off", // Несовместимо с prettier
+    "no-continue": "off",
+    "no-plusplus": "off",
+    "no-prototype-builtins": "off",
+    "no-underscore-dangle": "off",
+    "no-bitwise": "off",
+    "no-unused-expressions": "error",
+    "no-useless-escape": "warn",
+    "no-restricted-syntax": "warn",
+    "no-confusing-arrow": "error",
+    "no-return-assign": "warn",
+    "no-await-in-loop": "warn",
+    "no-use-before-define": "off", // правило присутствует в ts
+    "no-nested-ternary": "error",
+    "no-void": "off",
+    "no-useless-return": "off",
+    "no-console": ["warn", { allow: ["error", "info"] }],
+    "no-shadow": "off",
+    "no-param-reassign": "off",
+
+    // FORMATTERS
+    "prettier/prettier": [
+      "error",
+      {
+        tabWidth: 2,
+        useTabs: false,
+        singleQuote: false,
+        jsxSingleQuote: false,
+        bracketSameLine: false,
+        arrowParens: "avoid",
+        semi: true,
+        printWidth: 100,
+        endOfLine: "auto",
+      },
     ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
+  },
+
+  overrides: [
+    {
+      files: ["*.js"],
+      plugins: ["prettier"],
+      extends: ["eslint:recommended", "plugin:prettier/recommended"],
+    },
+    {
+      files: ["*.ts", "*.tsx", "*.d.ts"],
+      globals: {
+        JSX: true,
+      },
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        tsconfigRootDir: ".",
+        project: "tsconfig.json",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
         },
-        // override "simple-import-sort" config
-        {
-            "files": ["*.js", "*.jsx", "*.ts", "*.tsx"],
-            "rules": {
-                "simple-import-sort/imports": [
-                    "error",
-                    {
-                        "groups": [
-                            // Packages `react` related packages come first.
-                            ["^react", "^@?\\w"],
-                            // Internal packages.
-                            ["^(@|components)(/.*|$)"],
-                            // Side effect imports.
-                            ["^\\u0000"],
-                            // Parent imports. Put `..` last.
-                            ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
-                            // Other relative imports. Put same-folder imports and `.` last.
-                            ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
-                            // Style imports.
-                            ["^.+\\.?(css)$"]
-                        ]
-                    }
-                ]
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "ecmaFeatures": {
-            "jsx": true
-        }
-    },
-    "plugins": [
+        ecmaVersion: "latest",
+      },
+
+      plugins: [
+        "@typescript-eslint",
+        "react-hooks",
         "react",
-        'import',
+        "sort-imports-es6-autofix",
         "unused-imports",
-        "simple-import-sort",
-        "jsx-a11y",
-        'sonarjs',
-        "react-hooks"
-    ],
-    "rules": {
-        "@typescript-eslint/explicit-function-return-type":'off',
-        "@typescript-eslint/no-non-null-assertion":'off',
-        "@typescript-eslint/no-namespace":'off',
-        "@typescript-eslint/no-misused-promises":'off',
-        'unused-imports/no-unused-imports': 'error',
-        "simple-import-sort/imports": "error",
-        "simple-import-sort/exports": "error",
-        "react/jsx-indent": ["error", 2],
-        'react/static-property-placement': 'off',
-        'react/forbid-prop-types': 'off',
-        'react/require-default-props': 'off',
-        'react/no-unused-prop-types': 'off',
-        'prefer-destructuring': 'off',
-        'wrap-iife': 'off',
-        'spaced-comment': 'off',
-        'react/no-find-dom-node': 'off',
-        'react/no-did-mount-set-state': 'off',
-        'react/jsx-one-expression-per-line': 'off',
-        'react/no-this-in-sfc': 'warn',
-        'react/prop-types': 'off',
-        'react/jsx-boolean-value': 'off',
-        'react/destructuring-assignment': 'off',
-        'react/jsx-filename-extension': 'off',
-        'react/jsx-curly-newline': 'off',
-        'react/react-in-jsx-scope': 'off',
-        'react/no-array-index-key': 'off',
-        'react/jsx-wrap-multilines': 'off',
-        'react/prefer-stateless-function': 'off',
-        'react/no-children-prop': 'off',
-        'react/jsx-props-no-spreading': 'off',
-        'react/no-danger': 'off',
-        'react/function-component-definition': 'off',
-        "react/jsx-closing-tag-location": "error",
-        'react/jsx-pascal-case': [2, { ignore: ['UI', 'antd'] }],
-        "jsx-a11y/anchor-is-valid": ["error", {
-            "components": ["Link"],
-            "specialLink": ["to"]
-        }],
-        'jsx-a11y/click-events-have-key-events': 'off',
-        'jsx-a11y/label-has-associated-control': 'off',
-        'jsx-a11y/media-has-caption': 'off',
-        'jsx-a11y/no-static-element-interactions': 'off',
-        'jsx-a11y/no-noninteractive-element-interactions': 'off',
-        'jsx-a11y/control-has-associated-label': 'off',
-        'import/no-extraneous-dependencies': 'off',
-        'import/prefer-default-export': 'off',
-        'import/no-relative-packages': 'off',
-        'import/no-cycle': 'off',
-        'import/no-mutable-exports': 'warn',
-        'import/extensions': 'off',
-        'import/no-unresolved': 'off',
-        'sonarjs/no-duplicate-string': 'off',
-        'sonarjs/no-identical-functions': 'off',
-        'sonarjs/prefer-immediate-return': 'off',
-        "sonarjs/cognitive-complexity": "error",
-        "sonarjs/no-identical-expressions": "error"
+      ],
+      extends: [
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
+      ],
+      rules: {
+        // TYPESCRIPT
+        "@typescript-eslint/unbound-method": "off",
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/no-unnecessary-type-assertion": "warn",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-floating-promises": "off",
+        "@typescript-eslint/strict-boolean-expressions": "off",
+        /* запрещает использовать namspaces */
+        "@typescript-eslint/no-namespace": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/restrict-template-expressions": "off",
+        "@typescript-eslint/naming-convention": [
+          "warn",
+          {
+            selector: "interface",
+            format: ["PascalCase"],
+          },
+        ],
+
+        // REACT
+        "react/no-deprecated": "off",
+        "react/static-property-placement": "off",
+        "react/forbid-prop-types": "off",
+        "react/require-default-props": [1, { ignoreFunctionalComponents: true }],
+        "react/no-unused-prop-types": "off",
+        "react/no-find-dom-node": "off",
+        "react/no-did-mount-set-state": "off",
+        "react/jsx-one-expression-per-line": "off",
+        "react/no-this-in-sfc": "warn",
+        "react/prop-types": "off",
+        "react/jsx-boolean-value": "off",
+        "react/destructuring-assignment": "off",
+        "react/jsx-filename-extension": "off",
+        "react/jsx-curly-newline": "off",
+        "react/no-array-index-key": "off",
+        "react/jsx-wrap-multilines": "off",
+        "react/prefer-stateless-function": "off",
+        "react/no-children-prop": "off",
+        "react/jsx-props-no-spreading": "off",
+        "react/no-danger": "off",
+        "react/react-in-jsx-scope": "off",
+        "react/jsx-uses-react": "off",
+        "react/display-name": "warn",
+        "react/jsx-key": "warn",
+        "react/jsx-pascal-case": [2, { allowNamespace: true, ignore: ["UI", "samoletUI", "antD"] }],
+
+        // REACT-HOOKS
+        "react-hooks/exhaustive-deps": "warn",
+        "react-hooks/rules-of-hooks": "error",
+
+        // UNUSED-IMPORTS
+        "unused-imports/no-unused-imports": "warn",
+        "no-unused-vars": "off", // конфликтует с правилом ниже
+        "unused-imports/no-unused-vars": [
+          "warn",
+          {
+            vars: "all",
+            varsIgnorePattern: "^_",
+            args: "after-used",
+            argsIgnorePattern: "^_",
+          },
+        ],
+
+        // FORMATTERS
+        radix: "warn",
+        camelcase: "off", // deprecated use @typescript-eslint/naming-convention instead
+        strict: "warn",
+        quotes: "off",
+        eqeqeq: "warn",
+        indent: "off",
+
+        "sort-imports-es6-autofix/sort-imports-es6": [
+          2,
+          {
+            ignoreCase: false,
+            ignoreMemberSort: false,
+            memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
+          },
+        ],
+        "id-length": "off",
+      },
     },
-    "settings": {
-        "react": {
-            "version": "detect"
-        }
-    }
-}
+  ],
+};
