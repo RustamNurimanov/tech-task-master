3e0d48a9e6717b893c30a39ae1ce3fa16b6acba4
rustamnurimanov <r.damirovich@gmail.com>
2024-04-20 13:10:04 +0300

    [feature] added margin for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { Comments } from 'pages/comments'
+import { Comments } from "pages/comments";
+import styled from "styled-components";
 
-import { StyledBackground } from './styles'
-
+import forest from "./assets/background/forest.png";
 export const App = () => {
   return (
     <StyledBackground>
-      <Comments/>
+      <Comments />
     </StyledBackground>
-  )
-}
+  );
+};
+const StyledBackground = styled.main`
+  display: flex;
+  justify-content: center;
+  background-image: url(${forest});
+  background-color: ${({ theme }) => theme.background.B100};
+  height: fit-content;
+  min-height: 100dvh;
+  width: 100dvw;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+`;
