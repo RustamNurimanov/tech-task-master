3c7664f1f0f88101b6bf215bc986fa507ad3c7b5
rustamnurimanov <r.damirovich@gmail.com>
2024-04-20 14:29:44 +0300

    Revert "[chore] fix error msg"
    This reverts commit dc5381869d0f834ccccd25c455251f73b80ab0a0.

--- src/utils/onError.ts
+++ src/utils/onError.ts
@@ -1,11 +1,9 @@
-import { errors } from "../constants/errors";
 import axios from "axios";
 import toast from "react-hot-toast";
 
 export namespace ErrorTypes {
   export enum ErrorTypesEnum {
     NetworkError = "Network Error",
-    Unknown = "Unknown",
   }
 }
 
@@ -14,9 +12,9 @@ export namespace ErrorTypes {
 export const onError = (error: Error) => {
   if (axios.isAxiosError(error)) {
     if (error.message === ErrorTypes.ErrorTypesEnum.NetworkError) {
-      toast.error(errors[ErrorTypes.ErrorTypesEnum.NetworkError]);
+      toast.error("Произошла ошибка из за отсутствия подключения к интернету");
     }
     return;
   }
-  toast.error(errors[ErrorTypes.ErrorTypesEnum.Unknown]);
+  toast.error("Произошла неизвестная ошибка");
 };
